741fa665d4447a9e449625ddbf76e445bf01d114
Julian Lopez <juilopez@fi.uba.ar>
2025-04-26 11:00:09 -0300

    documentaciones

--- tdas/lista/lista.go
+++ tdas/lista/lista.go
@@ -21,23 +21,31 @@ type Lista[T any] interface {
 	// VerUltimo devuelve el ultimo elemento de la lista. Si la lista esta vacia, entra en pánico con un mensaje "La lista esta vacía".
 	VerUltimo() T
 
-	//Largo devuelve el largo de la lista, o lo que es equivalente, su cantidad de elementos.
+	// Largo devuelve el largo de la lista, o lo que es equivalente, su cantidad de elementos.
 	Largo() int
+
+	// Iterar recorre toda la lista aplicando la funcion visitar elemento a elemento hasta que finaliza la misma o hasta que visitar devuelva false.
 	Iterar(visitar func(T) bool)
+
+	// Iterador se encarga de inicializar el iterador externo en la primera posicion de la lista (el usuario es el encargado de realizar la iteracion a conveniencia).
 	Iterador() IteradorLista[T]
 }
 
 type IteradorLista[T any] interface {
 
-	//VerActual devuelve el dato de la posicion actual en la que se encuentra el iterador.
+	// VerActual devuelve el dato de la posicion actual en la que se encuentra el iterador.
 	VerActual() T
 
 	// HaySiguiente devuelve true si hay un siguiente elemento a leer en la posicion actual del iterador;de no ser asi devuelve false.
 	HaySiguiente() bool
 
-	// Siguiente mueve la posicion actual del iterador a la siguiente. Si no hay siguiente elemento a leer entra en panico
-	// con un mensaje "El iterador termino de iterar"
+	// Siguiente mueve la posicion actual del iterador a la del siguiente elemento. Si no hay siguiente elemento entra en panico
+	// con un mensaje "El iterador termino de iterar".
 	Siguiente()
+
+	// Insertar agrega un nuevo elemento en la posicion actual del iterador.
 	Insertar(T)
+
+	// Borrar elimina el elemento de la posicion actual del iterador. Si la lista no tiene elementos entra en panico con un mensaje "La lista esta vacia""
 	Borrar() T
 }
