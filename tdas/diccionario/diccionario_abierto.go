aa8dbe129c18c85ca2a86eaa974ac90d2578600b
Julian Lopez <juilopez@fi.uba.ar>
2025-05-03 11:54:21 -0300

    agrego constante

--- tdas/diccionario/diccionario_abierto.go
+++ tdas/diccionario/diccionario_abierto.go
@@ -5,6 +5,8 @@ import (
 	TDALista "tdas/lista"
 )
 
+const TAMAÑO_HASH = 20
+
 type parClaveValor[K comparable, V any] struct {
 	clave K
 	dato  V
@@ -23,11 +25,11 @@ type iterHash[K comparable, V any] struct {
 }
 
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
-	tabla := make([]TDALista.Lista[parClaveValor[K, V]], 5)
+	tabla := make([]TDALista.Lista[parClaveValor[K, V]], TAMAÑO_HASH)
 	for i := range tabla {
 		tabla[i] = TDALista.CrearListaEnlazada[parClaveValor[K, V]]()
 	}
-	return &hashAbierto[K, V]{tabla: tabla, tam: 5, cantidad: 0}
+	return &hashAbierto[K, V]{tabla: tabla, tam: TAMAÑO_HASH, cantidad: 0}
 }
 
 func (hash *hashAbierto[K, V]) Guardar(clave K, dato V) {
