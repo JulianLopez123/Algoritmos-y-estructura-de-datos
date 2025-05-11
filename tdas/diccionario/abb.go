ac40dbf5522e34bd04b5339dc885873c2b27a9aa
Julian Lopez <juilopez@fi.uba.ar>
2025-05-11 02:23:10 -0300

    crearABB

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -1,1 +1,21 @@
 package diccionario
+
+type abb[K comparable, V any] struct {
+	raiz     *nodoAb[K, V]
+	cantidad int
+	comparar func(K, K) int
+	// destruir dato¿?
+}
+
+type nodoAb[K comparable, V any] struct {
+	izq   *nodoAb[K, V]
+	der   *nodoAb[K, V]
+	clave K
+	dato  V
+}
+
+func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
+	return &abb[K, V]{raiz: nil, cantidad: 0, comparar: funcion_cmp}
+}
+
+
