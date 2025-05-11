8e8d03e04ab019fe33477f2aba3080c04666f25f
Julian Lopez <juilopez@fi.uba.ar>
2025-05-11 16:53:42 -0300

    Guardar

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -1,15 +1,14 @@
 package diccionario
 
 type abb[K comparable, V any] struct {
-	raiz     *nodoAb[K, V]
+	raiz     *nodoAbb[K, V]
 	cantidad int
 	comparar func(K, K) int
-	// destruir dato¿?
 }
 
-type nodoAb[K comparable, V any] struct {
-	izq   *nodoAb[K, V]
-	der   *nodoAb[K, V]
+type nodoAbb[K comparable, V any] struct {
+	izq   *nodoAbb[K, V]
+	der   *nodoAbb[K, V]
 	clave K
 	dato  V
 }
@@ -18,4 +17,28 @@ func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdena
 	return &abb[K, V]{raiz: nil, cantidad: 0, comparar: funcion_cmp}
 }
 
+func (abb *abb[K, V]) Cantidad() int{
+	return abb.cantidad
+}
 
+func (abb *abb[K, V]) Guardar(clave K, dato V) {
+	abb.raiz = abb.hallarPosicionDeNodo(clave,dato,abb.raiz)
+}
+
+
+func (abb *abb[K, V]) hallarPosicionDeNodo(clave K,dato V, nodo *nodoAbb[K,V]) *nodoAbb[K,V]{
+	if nodo == nil{
+		abb.cantidad++
+		return &nodoAbb[K, V]{clave:clave,dato:dato}
+	}
+	condicion := abb.comparar(clave,nodo.clave)
+	switch {
+	case condicion == 0:
+	nodo.dato = dato
+	case condicion > 0:
+	nodo.der = abb.hallarPosicionDeNodo(clave,dato,nodo.der)
+	case condicion < 0:
+	nodo.izq = abb.hallarPosicionDeNodo(clave,dato,nodo.izq) 
+	}
+	return nodo
+}
