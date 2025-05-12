05330ed1aa5d6a56c174051a8d103ee7000d49df
federico-fredotovich <ffredotovich@fi.uba.ar>
2025-05-11 23:47:06 -0300

    Hago primitiva pertenece

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -17,28 +17,45 @@ func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdena
 	return &abb[K, V]{raiz: nil, cantidad: 0, comparar: funcion_cmp}
 }
 
-func (abb *abb[K, V]) Cantidad() int{
+func (abb *abb[K, V]) Cantidad() int {
 	return abb.cantidad
 }
 
 func (abb *abb[K, V]) Guardar(clave K, dato V) {
-	abb.raiz = abb.hallarPosicionDeNodo(clave,dato,abb.raiz)
+	abb.raiz = abb.hallarPosicionDeNodo(clave, dato, abb.raiz)
 }
 
-
-func (abb *abb[K, V]) hallarPosicionDeNodo(clave K,dato V, nodo *nodoAbb[K,V]) *nodoAbb[K,V]{
-	if nodo == nil{
+func (abb *abb[K, V]) hallarPosicionDeNodo(clave K, dato V, nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
+	if nodo == nil {
 		abb.cantidad++
-		return &nodoAbb[K, V]{clave:clave,dato:dato}
+		return &nodoAbb[K, V]{clave: clave, dato: dato}
 	}
-	condicion := abb.comparar(clave,nodo.clave)
+	condicion := abb.comparar(clave, nodo.clave)
 	switch {
 	case condicion == 0:
-	nodo.dato = dato
+		nodo.dato = dato
 	case condicion > 0:
-	nodo.der = abb.hallarPosicionDeNodo(clave,dato,nodo.der)
+		nodo.der = abb.hallarPosicionDeNodo(clave, dato, nodo.der)
 	case condicion < 0:
-	nodo.izq = abb.hallarPosicionDeNodo(clave,dato,nodo.izq) 
+		nodo.izq = abb.hallarPosicionDeNodo(clave, dato, nodo.izq)
 	}
 	return nodo
+}
+
+func (abb *abb[K, V]) buscarClaveEnArbol(clave K, nodo *nodoAbb[K, V]) bool {
+	if nodo == nil {
+		return false
+	}
+	comparacion := abb.comparar(clave, nodo.clave)
+	if comparacion == 0 {
+		return true
+	} else if comparacion > 0 {
+		return abb.buscarClaveEnArbol(clave, nodo.izq)
+	} else {
+		return abb.buscarClaveEnArbol(clave, nodo.der)
+	}
+}
+
+func (abb *abb[K, V]) Pertenece(clave K) bool {
+	return abb.buscarClaveEnArbol(clave, abb.raiz)
 }
