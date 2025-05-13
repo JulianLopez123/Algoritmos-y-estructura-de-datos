c545d6ef097f3b08e986e57a2a2525d07b635ea5
federico-fredotovich <ffredotovich@fi.uba.ar>
2025-05-12 22:18:20 -0300

    Hago funcion obtener e iterador externo

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -1,5 +1,9 @@
 package diccionario
 
+import (
+	TDAPila "tdas/pila"
+)
+
 type abb[K comparable, V any] struct {
 	raiz     *nodoAbb[K, V]
 	cantidad int
@@ -11,6 +15,10 @@ type nodoAbb[K comparable, V any] struct {
 	der   *nodoAbb[K, V]
 	clave K
 	dato  V
+}
+
+type iterAbb[K comparable, V any] struct {
+	pila TDAPila.Pila[*nodoAbb[K, V]]
 }
 
 func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
@@ -25,6 +33,13 @@ func (abb *abb[K, V]) Pertenece(clave K) bool {
 	return abb.buscarClaveEnArbol(clave, abb.raiz)
 }
 
+func (abb *abb[K, V]) Obtener(clave K) V {
+	if !abb.Pertenece(clave) {
+		panic("La clave no pertenece al diccionario")
+	}
+	return abb.buscarDatoEnArbol(clave, abb.raiz)
+}
+
 func (abb *abb[K, V]) Borrar(clave K) V {
 	var dato V
 	abb.raiz, dato = abb.borrar(clave, abb.raiz)
@@ -34,6 +49,42 @@ func (abb *abb[K, V]) Borrar(clave K) V {
 
 func (abb *abb[K, V]) Cantidad() int {
 	return abb.cantidad
+}
+
+func (abb *abb[K, V]) Iterador() IterDiccionario[K, V] {
+	pila := TDAPila.CrearPilaDinamica[*nodoAbb[K, V]]()
+	iter := &iterAbb[K, V]{pila: pila}
+
+	for abb.raiz != nil {
+		pila.Apilar(abb.raiz)
+		abb.raiz = abb.raiz.izq
+	}
+
+	return iter
+}
+
+func (iter *iterAbb[K, V]) HaySiguiente() bool {
+	return iter.pila.EstaVacia()
+}
+
+func (iter *iterAbb[K, V]) VerActual() (K, V) {
+	if !iter.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+	return iter.pila.VerTope().clave, iter.pila.VerTope().dato
+}
+
+func (iter *iterAbb[K, V]) Siguiente() {
+	if !iter.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+
+	nodo_eliminado := iter.pila.Desapilar()
+	nodo_actual := nodo_eliminado.der
+	for nodo_actual != nil {
+		iter.pila.Apilar(nodo_actual)
+		nodo_actual = nodo_actual.izq
+	}
 }
 
 func (abb *abb[K, V]) hallarPosicionDeNodo(clave K, dato V, nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
@@ -64,6 +115,20 @@ func (abb *abb[K, V]) buscarClaveEnArbol(clave K, nodo *nodoAbb[K, V]) bool {
 		return abb.buscarClaveEnArbol(clave, nodo.izq)
 	} else {
 		return abb.buscarClaveEnArbol(clave, nodo.der)
+	}
+}
+
+func (abb *abb[K, V]) buscarDatoEnArbol(clave K, nodo *nodoAbb[K, V]) V {
+	if nodo == nil {
+		panic("La clave no pertenece al diccionario")
+	}
+	comparacion := abb.comparar(clave, nodo.clave)
+	if comparacion == 0 {
+		return nodo.dato
+	} else if comparacion < 0 {
+		return abb.buscarDatoEnArbol(clave, nodo.izq)
+	} else {
+		return abb.buscarDatoEnArbol(clave, nodo.der)
 	}
 }
 
