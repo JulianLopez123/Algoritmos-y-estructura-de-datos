d97073f7e38d2d82f32a7647e30d1aa5e547443c
Julian Lopez <juilopez@fi.uba.ar>
2025-05-26 03:20:44 -0300

    heapify, downheap

--- tdas/heap/heap.go
+++ tdas/heap/heap.go
@@ -11,7 +11,9 @@ func CrearHeap[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
 }
 
 func CrearHeapArr[T any](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
-	return &colaConPrioridad[T]{datos: arreglo, cant: len(arreglo), cmp: funcion_cmp}
+	cola := &colaConPrioridad[T]{datos: arreglo, cant: len(arreglo), cmp: funcion_cmp}
+	cola.heapify(cola.datos,cola.cant,cola.cmp)
+	return cola
 }
 
 func (heap *colaConPrioridad[T]) EstaVacia() bool {
@@ -21,7 +23,7 @@ func (heap *colaConPrioridad[T]) EstaVacia() bool {
 func (heap *colaConPrioridad[T]) Encolar(elemento T) {
 	heap.cant++
 	heap.datos[heap.cant-1] = elemento
-	heap.heapify(heap.datos, heap.cant-1)
+	heap.heapify(heap.datos, heap.cant-1,heap.cmp)
 }
 
 func (heap *colaConPrioridad[T]) VerMax() T {
@@ -29,23 +31,59 @@ func (heap *colaConPrioridad[T]) VerMax() T {
 }
 
 func (heap *colaConPrioridad[T]) Desencolar() T {
+	if heap.EstaVacia(){
+		panic("La cola esta vacia")
+	}
+	dato_eliminado:= heap.datos[0]
+	heap.datos[0], heap.datos[heap.cant-1] = heap.datos[heap.cant-1], heap.datos[0]
+	heap.cant--
+	heap.downHeap(0)
+	return dato_eliminado
 }
 
 func (heap *colaConPrioridad[T]) Cantidad() int {
 	return heap.cant
 }
 
-func (heap *colaConPrioridad[T]) heapify(arr []T, i int) {
-	nodo_padre := (i - 1) / 2
-	comparacion := heap.cmp(arr[i], arr[nodo_padre])
-	if comparacion == 0 {
+func (heap *colaConPrioridad[T]) heapify(arr []T, largo int, cmp func(T, T) int) {
+	if largo == 0{
+		return 
+	}
+	heap.datos = arr
+	heap.cant = largo
+	heap.cmp = cmp
+	
+	ultimo_nodo_con_hijos := (largo /2) -1
+	for i := ultimo_nodo_con_hijos; i > -1;i--{
+		heap.downHeap(i)
+	}
+}
+
+func (heap *colaConPrioridad[T]) downHeap(posicion_padre int){
+	pos_hijo_izq := 2 * posicion_padre + 1
+	pos_hijo_der := 2 * posicion_padre + 2
+	if pos_hijo_izq >= heap.cant{
 		return
 	}
-	if comparacion > 0 {
-		arr[i], arr[nodo_padre] = arr[nodo_padre], arr[i]
-		heap.heapify(arr, nodo_padre)
-	}
-	if comparacion < 0 {
+	padre := heap.datos[posicion_padre]
+	hijo_izq := heap.datos[pos_hijo_izq]
+	
+	if pos_hijo_der >= heap.cant{
+		if heap.cmp(hijo_izq,padre) > 0{
+			heap.datos[posicion_padre], heap.datos[pos_hijo_izq] = heap.datos[pos_hijo_izq], heap.datos[posicion_padre]
+		}
 		return
+	}
+
+	hijo_der := heap.datos[pos_hijo_der]
+
+	if heap.cmp(hijo_der,padre) > 0 ||  heap.cmp(hijo_izq,padre) > 0{
+		if heap.cmp(hijo_der,hijo_izq) > 0{
+			heap.datos[posicion_padre], heap.datos[pos_hijo_der] = heap.datos[pos_hijo_der], heap.datos[posicion_padre]
+			heap.downHeap(pos_hijo_der)
+		}else{
+			heap.datos[posicion_padre], heap.datos[pos_hijo_izq] = heap.datos[pos_hijo_izq], heap.datos[posicion_padre]
+			heap.downHeap(pos_hijo_izq)
+		}
 	}
 }
